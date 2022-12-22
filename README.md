113829c6d048e82f7ddb4655a529e8246cde3fd2
Alejandro FR <alejandro.figueroa.rodriguez.01@gmail.com>
2022-12-21 21:46:15 -0500

    Rectificacion de errores en el middleware de multer

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-#Notas
+#  Versiones:
 
+### v0.0.3:
+- Tiene el formulario del frontend para agregar categorias con toda la informacion que necesita.
+- Envia el formulario con toda la informacion al servidor por un formData y fetch.
+- El servidor recive la informacion y archiva la imagen.
+- Guarda la imagen en carpetas personalizadas con el nombre de la categoria ordenadamente.
+
+### v0.0.31:
+- Se modificó el middleware de multer para verificar si existen las carpetas antes de crearlas.
+- Se cambió los inputs multiples de las fotos a solo 1 foto.
+- Se ignoró la carpeta uploads/.
+
+### v0.0.32:
+- Se arreglaron errores en el middleware de multer al crear carpetas.
--- middlewares/almacenarLaImagenEnCarpetas.js
+++ middlewares/almacenarLaImagenEnCarpetas.js
@@ -6,26 +6,24 @@ module.exports = categoria  => {
     const storage = multer.diskStorage({
         destination: async ( req, file, cb ) => {
             // Crea la carpeta de la Categoria especifica automatico
-            if(fs.existsSync(`/../uploads/${categoria}` == false)){
-                console.log("no existe la carpeta peliculas");
+            console.log(fs.existsSync( path.join( __dirname, `/../uploads/${categoria}`) ));
+            if( fs.existsSync( path.join( __dirname, `/../uploads/${categoria}`) ) ) {
+                console.log("existe la carpeta peliculas");
+                if( fs.existsSync(  path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`) ) == false ) {
+                    console.log("No Existe la carpeta con el nombre " + req.body.nombre);
+                    // Crea la carpeta de la Pelicula con su nombre
+                    await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {})
+                }
+            // Caso de que no exita la carpeta de la categoria
+            }else{
+                console.log("no existe la carpeta " + categoria);
                 await fs.mkdir( path.join( __dirname, `/../uploads/${categoria}`), err =>  {});
                 await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {})
-                cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
-
-            }else{
-                console.log("existe la carpeta peliculas");
-                // Crea la carpeta de la Pelicula con su nombre
-                if( fs.existsSync(`/../uploads/Peliculas/${req.body.nombre}`) == false ) {
-                    console.log("no existe la carpeta " + req.body.nombre);
-                    await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {})
-                    // Guarda la imagen dentro de la carpeta de la pelicula
-                    cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
-                }else{
-                    // Guarda la imagen dentro de la carpeta de la pelicula
-                    cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
-                    console.log("Ya Existe una carpeta con el nombre " + req.body.nombre);
-                }
             }
+
+            // Guarda la imagen dentro de la carpeta de la pelicula
+            cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
+
         }, 
         filename: ( req, file, cb ) => {
             const fecha = new Date();
