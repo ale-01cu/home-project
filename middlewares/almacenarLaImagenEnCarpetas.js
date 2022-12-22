73f6dadd4801fdda588ee7f65236b7659eaba621
Alejandro FR <alejandro.figueroa.rodriguez.01@gmail.com>
2022-12-21 20:24:13 -0500

    Modifique el middleware de multer

--- middlewares/almacenarLaImagenEnCarpetas.js
+++ middlewares/almacenarLaImagenEnCarpetas.js
@@ -6,15 +6,26 @@ module.exports = categoria  => {
     const storage = multer.diskStorage({
         destination: async ( req, file, cb ) => {
             // Crea la carpeta de la Categoria especifica automatico
-            await fs.mkdir( path.join( __dirname, `/../uploads/${categoria}`), err =>  {
-                if (err) console.log(`Ya Existe la carpeta ${categoria}...!!!`);
-            })
-            // Crea la carpeta de la Pelicula con su nombre
-            await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {
-                if (err) console.log("Ya Existe la Carpeta " + req.body.nombre);
-            })
-            // Guarda la imagen dentro de la carpeta de la pelicula
-            cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
+            if(fs.existsSync(`/../uploads/${categoria}` == false)){
+                console.log("no existe la carpeta peliculas");
+                await fs.mkdir( path.join( __dirname, `/../uploads/${categoria}`), err =>  {});
+                await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {})
+                cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
+
+            }else{
+                console.log("existe la carpeta peliculas");
+                // Crea la carpeta de la Pelicula con su nombre
+                if( fs.existsSync(`/../uploads/Peliculas/${req.body.nombre}`) == false ) {
+                    console.log("no existe la carpeta " + req.body.nombre);
+                    await fs.mkdir( path.join( __dirname, `/../uploads/Peliculas/${req.body.nombre}`), err => {})
+                    // Guarda la imagen dentro de la carpeta de la pelicula
+                    cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
+                }else{
+                    // Guarda la imagen dentro de la carpeta de la pelicula
+                    cb(null, path.join(__dirname, `/../uploads/Peliculas/${req.body.nombre}`));
+                    console.log("Ya Existe una carpeta con el nombre " + req.body.nombre);
+                }
+            }
         }, 
         filename: ( req, file, cb ) => {
             const fecha = new Date();
