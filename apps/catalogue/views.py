8327d191f2bd133585513bc681fdf76b41ae3a8f
Alejandro Figueroa <alejandro.figueroa.rodriguez.01@gmail.com>
2023-07-12 23:32:51 -0600

    Se agego el buscador en el backend y los campos donde buscar

--- apps/catalogue/views.py
+++ apps/catalogue/views.py
@@ -2,10 +2,22 @@ from rest_framework import generics
 from .serializers import ContentDetailSerializer, ContentListSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import ContentPagination
+from rest_framework.filters import SearchFilter
 
 class ContentListAPIView(generics.ListAPIView):
     serializer_class = ContentListSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = [
+        'name', 
+        'description', 
+        'category__name',
+        'release_date',
+        'release_year',
+        'platform',
+        'countrie',
+        'genders__name',
+        'actors__full_name'
+    ]
     filterset_fields = ['category__name']
     pagination_class = ContentPagination
     
