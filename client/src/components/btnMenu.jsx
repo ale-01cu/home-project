d7506b033e4dfa14e00e1afa2834c45466aaeae0
Alejandro Figueroa <alejandro.figueroa.rodriguez.01@gmail.com>
2023-07-15 20:14:45 -0600

    Se agregaron los tooltips al menu y se creo un nuevo componento para esto botones del menu

---
+++ client/src/components/btnMenu.jsx
@@ -0,0 +1,49 @@
+import { ControlledMenu, MenuItem, useHover, useMenuState } from '@szhsin/react-menu';
+import {Link} from 'react-router-dom'
+import {useRef} from 'react'
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
+
+
+export const BtnMenu = ({ logo, path='', menuItemClassName='', text='', isList=false, list=[], titleList='', BtnClassName='', menuItemClassNameList='' }) => {
+  const ref = useRef(null);
+  const [menuState, toggle] = useMenuState({ transition: true, transitionTimeout: '0.1s' });
+  const { anchorProps, hoverProps } = useHover(menuState.state, toggle);
+
+
+  return (
+    <>
+      <Link ref={ref} {...anchorProps} to={path} className={BtnClassName}>
+        <img src={logo} alt="" width={30} height={30}/>
+      </Link>
+
+      <ControlledMenu
+        {...hoverProps}
+        {...menuState}
+        anchorRef={ref}
+        onClose={() => toggle(false)}
+        direction='right'
+        className={'hidden sm:block'}
+      >
+        {
+          isList
+          ? <>
+              <MenuItem className={menuItemClassName}>{titleList}</MenuItem>
+              {list.map(e => (
+                <MenuItem key={e.id} className={menuItemClassNameList}>
+                  <Link to={'/' + e.name} className='w-full'>
+                    {e.name}
+                  </Link>
+                </MenuItem>
+              ))}
+            </>
+          : <MenuItem className={menuItemClassName}>
+              {text}
+            </MenuItem>
+        }
+
+      </ControlledMenu>
+    </>
+    
+  )
+}
